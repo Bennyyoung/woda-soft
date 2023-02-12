af8d612a42214f203bc85648addd796d1a241d18
monsaw <benjamineffiong124@yahoo.com>
2023-02-12 21:30:33 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'uikit/dist/css/uikit.min.css';
+
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+// loads the Icon plugin
+UIkit.use(Icons);
+// components can be called from the imported UIkit reference
+//UIkit.notification('Hello world.');
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
